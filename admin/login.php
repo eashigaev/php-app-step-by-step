717afbafe40913ed0fd8fba9c81a0923ebeee419
Eugeny Shigaev <eugeny85@gmail.com>
2020-03-10 20:45:09 +0400

    Login fix

--- admin/login.php
+++ admin/login.php
@@ -7,7 +7,7 @@ $config = require_once '../config.php';
 if (isset($_POST)) {
     $login = $_POST['login'] ?? null;
     $pass = $_POST['pass'] ?? null;
-    if ($login === $config['user.login'] && md5($pass === $config['user.pass'])) {
+    if ($login === $config['user.login'] && md5($pass) === $config['user.pass']) {
         $_SESSION['auth'] = true;
         header('Location: ./');
         exit;
