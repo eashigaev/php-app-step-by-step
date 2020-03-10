118d934a4cbdee5380b3bcab80e4c2c801f7befe
Eugeny Shigaev <eugeny85@gmail.com>
2020-03-11 03:41:17 +0400

    Config clean

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php
 
 return [
-    'templates.path' => __DIR__ . '/templates/',
-
-    'user.login' => 'admin',
-    'user.pass' => md5('12345'),
+    'templates.path' => __DIR__ . '/templates/'
 ];
